58cfdd5a619ebc6087046cb94d5749fb411ec566
Sneha Dhingra <snehadhingra0987@gmail.com>
2025-06-19 15:51:46 +0530

    d2

---
+++ index.js
@@ -0,0 +1,79 @@
+// data types and variables
+
+
+// console.log("hello world");
+// let abc="the weather is very hot"
+// console.log(abc);
+// let add=5+2
+// console.log(add)
+
+// let age="20"
+// let a = 20
+// let result = Number(age) + a
+// console.log(result)
+
+
+// operators
+// let no=20
+// if(no>=50)
+// {
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+// // odd or even 
+
+// let Num = 39
+// if(Num%2 == 0)
+// {
+//     console.log("No is even")
+// }else{
+//     console.log("No is odd")
+// }
+
+// // no is largest or smallest
+//  let a=40 ,b=20 ,c=50
+//  if(a<b && a<c){
+//     console.log("a")
+//  }
+//  else if(b>a &&b>c){
+//     console.log("b")
+//  }
+
+//  else{
+//     console.log("c")
+//  }
+
+//  SWITCH STATEMENT
+let day = 7
+let dayName
+switch (day) {
+    case 1:
+dayName = "monday";
+break;
+
+     case 2:
+dayName = "tuesday";
+break;
+
+     case 3:
+dayName = "wednesday";
+break;
+
+     case 4:
+dayName = "thrusday";
+break;
+     case 5:
+dayName = "friday";
+break;
+    default:
+dayName = "Invalid";
+}
+console.log("day:", dayName);
+
+// For Loop
+
+
+
+
